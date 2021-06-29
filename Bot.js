7995259d0e2d2979e93357773731121785e3efb4
Dev Ahuja <72222819+devahuja811@users.noreply.github.com>
2021-06-30 01:38:03 +0530

    Added music features

--- Bot.js
+++ Bot.js
@@ -1,17 +1,38 @@
 const Dotenv = require("dotenv")
 Dotenv.config()
 const Discord = require("discord.js")
+const ytdl = require("ytdl-core")
 const client = new Discord.Client()
 
 client.login(process.env.TOKEN)
 // client.on("guildMemberAdd", function(Member){
 //     client
 // })
-client.on('message', message => {
+client.on('message', async message => {
+    let connection;
+    if (message.author.bot) return;
     if(message.content == "Bruh"){
         return message.channel.send("Bruh to you too")
+    }
+    if(message.content.toLowerCase() == "join"){
+        if(message.member.voice.channel){
+            connection = await message.member.voice.channel.join()
+        }
+    }
+    if(message.content.toLowerCase().startsWith("play")){
+        if(!connection){
+            connection = await message.member.voice.channel.join()
+            
+        }
+        connection.play(ytdl(message.content.slice(5)))
+        
+    }
+    if(message.content.toLowerCase() == "disconnect"){
+        connection.dispatcher.destroy()
+        connection.disconnect()
     }
 });
 client.on('ready', () => {
     console.log("Bot is ready");
 });
+
