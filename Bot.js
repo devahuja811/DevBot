caf5faf97e548862fa883a12c280b4ec8abc4008
Dev Ahuja <72222819+devahuja811@users.noreply.github.com>
2021-06-30 18:18:55 +0530

    Added UMass People Finder Feature

--- Bot.js
+++ Bot.js
@@ -1,38 +1,74 @@
-const Dotenv = require("dotenv")
-Dotenv.config()
-const Discord = require("discord.js")
-const ytdl = require("ytdl-core")
-const client = new Discord.Client()
+const Dotenv = require("dotenv");
+const fetch = require("node-fetch");
+Dotenv.config();
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+const client = new Discord.Client();
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 // client.on("guildMemberAdd", function(Member){
 //     client
 // })
-client.on('message', async message => {
-    let connection;
-    if (message.author.bot) return;
-    if(message.content == "Bruh"){
-        return message.channel.send("Bruh to you too")
+client.on("message", async (message) => {
+  let connection;
+  if (message.author.bot) return;
+  if (message.content == "Bruh") {
+    return message.channel.send("Bruh to you too");
+  }
+  if (message.content.toLowerCase() == "join") {
+    if (message.member.voice.channel) {
+      connection = await message.member.voice.channel.join();
     }
-    if(message.content.toLowerCase() == "join"){
-        if(message.member.voice.channel){
-            connection = await message.member.voice.channel.join()
+  }
+  if (message.content.toLowerCase().startsWith("play")) {
+    if (!connection) {
+      connection = await message.member.voice.channel.join();
+    }
+    connection.play(ytdl(message.content.slice(5)));
+  }
+  if (message.content.toLowerCase() == "disconnect") {
+    connection.dispatcher.destroy();
+    connection.disconnect();
+  }
+  if (message.content.toLowerCase().startsWith("find")) {
+    let name = message.content.slice(5);
+    fetch(process.env.PEOPLE_FINDER, {
+      method: "POST",
+      body: new URLSearchParams({
+        q: name,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.Results.length > 0) {
+          let embed = new Discord.MessageEmbed()
+            .setTitle(`Search: ${name}`)
+            .setDescription("From UMass People Finder")
+            .setColor("#FF0000")
+            .addFields(
+              data.Results.map((result) => {
+                if (!result.Major) {
+                  result.Major = ["Faculty/Staff"];
+                }
+                return {
+                  name: result.Name,
+                  value: `                  
+                  Major: ${result.Major.join("/")}
+                  Email: ${result.Email}
+                  `,
+                };
+              })
+            );
+          return message.channel.send(embed);
+        } else {
+          return message.channel.send("No ID found");
         }
-    }
-    if(message.content.toLowerCase().startsWith("play")){
-        if(!connection){
-            connection = await message.member.voice.channel.join()
-            
-        }
-        connection.play(ytdl(message.content.slice(5)))
-        
-    }
-    if(message.content.toLowerCase() == "disconnect"){
-        connection.dispatcher.destroy()
-        connection.disconnect()
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } //!info
 });
-client.on('ready', () => {
-    console.log("Bot is ready");
+client.on("ready", () => {
+  console.log("Bot is ready");
 });
-
